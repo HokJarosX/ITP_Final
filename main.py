3a09dfcde5b1f4d1bf13813a927befa447e14c7b
Ярослав <hokjarosx@gmail.com>
2026-05-19 21:02:46 +0500

    for me

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from input.parser import load
 from services.expences import catergories_expenses
+
 def main():
     tr = load("data/transaction.json")
 
     report = catergories_expenses(tr)
-    print(report)
 
-    print(tr)
+    print(f"All transactions: {tr}")
 
+    print(f"Часть Абыля(категории):{report}")
 
 main()
