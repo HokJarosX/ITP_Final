310109e76f13e29737d400fe3affb6a5be1ec10f
Abil Yelaman <abilelaman607@gmail.com>
2026-05-19 07:41:29 +0500

    Initial commit: full expense tracking project

--- classes/transactions.py
+++ classes/transactions.py
@@ -5,3 +5,7 @@ class Transaction:
         self.amount = amount
         self.date = date
 
+    def __repr__(self):
+        cat = self.category if self.category else "No category"
+        return f"({self.date} | {self.type.upper()} | {cat} | {self.amount}$)"
+
