0b58050e20a1306533b7696a969cdb6b6f2ffe56
sailaudinmuhammed-cmyk <sailau.dinmuhammed@gmail.com>
2026-05-20 08:22:59 +0500

    Made transactions to have encapsulation. Added getters and replaced old tr.amount or tr.type and etc with tr.getType() and so on

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
+from UI import UserInterface
 from input.parser import load
 from services.expences import catergories_expenses
+import UI
 def main():
-    tr = load("data/transaction.json")
-
-    report = catergories_expenses(tr)
-    print(report)
-
-    print(tr)
-
+    UserInterface()
 
 main()
