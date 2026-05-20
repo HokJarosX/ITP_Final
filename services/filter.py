6019dd399012aa7ecedf96fd328e72489efca5c0
sailaudinmukhammed <sailau.dinmuhammed@gmail.com>
2026-05-20 18:55:39 +0500

    Made some changes in filter.py

--- services/filter.py
+++ services/filter.py
@@ -1,16 +1,23 @@
-class filter():
-    def __init__(self, goal = "", transcriptions = None, where = ""):
+from abc import ABC, abstractmethod
+
+class Filter(ABC):
+    def __init__(self, transcriptions = None, goal = ""):
+        self._transcriptions = transcriptions or []
         self._goal = goal
-        self._transcription = transcriptions
-        self._where = where
+
+    @abstractmethod
+    def apply(self):
+        pass
+
+
+
+class FilterCategory(Filter):
+    def __init__(self, transcriptions = None, goal = ""):
+        super().__init__(transcriptions, goal)
 
     def apply(self):
         result = []
-        for transcription in self._transcription:
-            if getattr(transcription, self._where) == self._goal:
+        for transcription in self._transcriptions:
+            if transcription.getCategory() == self._goal:
                 result.append(transcription)
         return result
-
-class filterCategory(filter):
-    def __init__(self, goal = "", transcriptions = None):
-        super().__init__(goal, transcriptions, where = "_category")
