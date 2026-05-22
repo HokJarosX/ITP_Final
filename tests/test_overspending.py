17f25da6eaa8fb63b75580e8e4c88555af61e754
Kamila Ahmadzada <255888@astanait.edu.kz>
2026-05-23 01:30:40 +0600

    added a test in test_over

--- tests/test_overspending.py
+++ tests/test_overspending.py
@@ -1,10 +1,26 @@
-import sys
-import os
-sys.path.insert(0,os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from classess.transactions import Transaction
-from services.overspendig import detect_overspending
 
-def make_expense(catagory, amount):
-    return Transaction(type="expense",):
-def make_income(amount):
-    return Transaction(type=)
+import os
+
+import sys
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from classes.transactions import Transaction
+from  services.overspending import detect_overspending
+
+def make_expense (catagory, amount,):
+    return Transaction(type ="expense", category=catagory, amount=amount , date ="2026-05-01")
+
+def test_overspending():
+
+    result = detect_overspending([make_expense("food", 80)],
+        {"food": 50} )
+
+    assert result["food"]["over_by"]==30
+    print("test 1 passed correctly")
+
+
+
+if __name__=="__main__":
+    test_overspending()
+
